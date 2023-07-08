ad779e5afb84bf5201be7fcc72633e8704976ae5
Venca321 <login@vaclavparma.cz>
2023-07-08 12:55:50 +0200

    First commit

---
+++ sveltekit/src/routes/+page.server.ts
@@ -0,0 +1,12 @@
+import type { PageServerLoad } from "./$types";
+
+const key = "3028397c9ba109fe21b8aabb61"
+
+export const load: PageServerLoad = async () => {
+    const response = await fetch("http://ghost:2368/ghost/api/content/posts?key="+key+"&include=tags,authors", {
+        method: 'GET',
+    });
+    const data = await response.json();
+
+    return {posts: data}
+}
---
+++ sveltekit/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let data;
+	let posts = data.posts.posts;
+</script>
+
+{#each posts as post}
+	<div>
+		<img src={post.feature_image} alt={post.feature_image_alt} />
+		<h2>{post.title}</h2>
+		<p>{post.authors[0].name}</p>
+		<p>{post.updated_at}</p>
+		<p>{post.reading_time}</p>
+		{#each post.tags as tag}
+			<p>{tag.name}</p>
+		{/each}
+		<p>{post.excerpt}</p>
+		<a href="/posts/{post.slug}">Odkaz</a>
+	</div>
+{/each}
